d328af2ca4fc2054afc8f80baac55d9555cdd9e7
Ash Shetty <avshetty1980@gmail.com>
2021-10-12 16:13:54 +0100

    first commit with working single Apache webserver

---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+aws_access_key = "AKIA476ZKH6SHJXPCA7E"
+
+aws_secret_key = "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"
+
+#ssh_key = ""
---
+++ vpc.tf
@@ -0,0 +1,129 @@
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+
+provider "aws" {  
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region = "eu-west-2"
+}
+
+variable "ssh_key" {}
+
+resource "tls_private_key" "web_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = var.ssh_key
+  public_key = tls_private_key.web_key.public_key_openssh
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name      = "test-vpc"
+    ManagedBy = "terraform"
+  }
+}
+
+resource "aws_subnet" "subnet_1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-west-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name      = "subnet_1"
+    ManagedBy = "terraform"
+  }
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name      = "test-igw"
+    ManagedBy = "terraform"
+  }
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name      = "route-table"
+    ManagedBy = "terraform"
+  }
+}
+
+resource "aws_route" "route_to_gateway" {
+  route_table_id         = aws_route_table.rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.internet_gateway.id
+  depends_on             = [aws_route_table.rt]
+}
+
+resource "aws_route_table_association" "subnet_1" {
+  subnet_id      = aws_subnet.subnet_1.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_security_group" "web_server" {
+  name        = "web_server"
+  description = "Allow standard HTTP, HTTPS, SSH and everything out"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-403e2524"
+  instance_type = "t2.micro" 
+  monitoring    = false      
+  user_data     = "#!/bin/bash\nyum update -y\nyum install -y httpd24\nservice httpd start"
+  subnet_id     = aws_subnet.subnet_1.id
+  key_name      = aws_key_pair.generated_key.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.web_server.id,
+  ]
+
+  tags = {
+    Name = "web"
+  }
+}
+
+output "instance_dns" {
+  value = aws_instance.web.public_dns
+}
